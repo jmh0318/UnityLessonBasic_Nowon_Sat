b93db62e5ea85b4edc16f9454d8a0a1fd35df76d
jmh0318 <tansansu120@gmail.com>
2022-01-22 19:00:16 +0900

    2022_01_22
    1. Program class
    2. variable
    3. if
    4. method

---
+++ UnityLesson_CSharp/UnityLesson_CShop_ProgramClass_Dolnets/Program.cs
@@ -0,0 +1,16 @@
+﻿// 파란친구들 : 키워드 , 예약어
+using System;
+// using 키워드
+// using 은 뭔가 사용하겠다 라는 뜻
+// 1. 미리 짜여진 코드 덩어리
+// 2. 우선순위 차순위인 함수르 일반적으로 호출할 때
+namespace UnityLesson_CSharp_ProgramClass_Dotnet5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
